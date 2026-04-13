caadc52a1ba7016b3e7a26b7bece74faaae4b619
WendolynQuartey <quarteywendolyn@gmail.com>
2026-04-13 10:27:10 -0400

    user's full name concatingating

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name below:");
+        System.out.print("\nFirst Name: ");
+        String fullName = scanner.nextLine();
+        System.out.print("\nMiddle Name: ");
+        fullName += " " + scanner.nextLine();
+        System.out.print("\nLast Name: ");
+        fullName += " " + scanner.nextLine();
+        System.out.print("\nSuffix: ");
+        fullName += " " + scanner.nextLine();
+
+        System.out.println(fullName);
+    }
+}
